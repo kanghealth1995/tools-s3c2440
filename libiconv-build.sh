4ebbbe74fc2f798db5f26832618bde787c0e5654
kang <15230136190@163.com>
2025-11-23 22:30:41 +0800

    add libiconv 1.14 tool and build script.

--- libiconv-build.sh
+++ libiconv-build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-rm -rf libiconv-1.18/
-tar -zxvf libiconv-1.18.tar.gz
+rm -rf libiconv-1.14/
+tar -zxvf libiconv-1.14.tar.gz
 
-cd libiconv-1.18/
+cd libiconv-1.14/
 mkdir _install
-./configure --host=arm-linux --prefix=$(pwd)/_install
+./configure --host=arm-none-linux-gnueabi --prefix=$(pwd)/_install CC=arm-none-linux-gnueabi-gcc LDFLAGS="-L/usr/local/arm/4.4.3/arm-none-linux-gnueabi/sys-root/lib" --enable-static
 make
 make install
